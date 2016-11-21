b28faa9ddb310b89387d9cb7b98e59b20f4ce66e
alhazza944 <alhazza944@gmail.com>
2016-11-20 21:38:19 -0500

    Add insert Image to DB

--- AdminEditEmployee.php
+++ AdminEditEmployee.php
@@ -8,6 +8,7 @@ mysql_select_db("hospital",$query);
 if(isset($_GET['Edit']))
 {
 $EmpID=$_GET['Edit'];
+$query1=mysql_query("select * from employee WHERE EmpID = '$EmpID'");
 echo $EmpID;
 $_SESSION['EmpID'] = $EmpID;
 if(isset($_POST['submit']))
@@ -17,24 +18,27 @@ $Lname=$_POST['Lname'];
 $PhoneNumber=$_POST['PhoneNumber'];
 $Salary=$_POST['Salary'];
 $JobTitle=$_POST['JobTitle'];
+        $image= addslashes($_FILES['image']['tmp']);
+		$name= addslashes($_FILES['image']['name']);
+		$image= file_get_contents($image);
+		$image= base64_encode($image);
 
-$query3=mysql_query("update employee set Fname = '$Fname', Lname = '$Lname', PhoneNumber = '$PhoneNumber', Salary='$Salary',  JobTitle = '$JobTitle' where EmpID='$EmpID'");
-
+$query3=mysql_query("update employee set Fname = '$Fname', Lname = '$Lname', PhoneNumber = '$PhoneNumber', Salary='$Salary',  JobTitle = '$JobTitle' , image = '$image' , name = '$name' where EmpID='$EmpID'");
 if($query3)
 {
 header('location:AdminListEmployee.php');
 }
 }
-$query1=mysql_query("select * from employee WHERE EmpID = '$EmpID'");
+
 $query2=mysql_fetch_array($query1);
 ?>
-<form method="post" action="">
+<form method="post" action="" enctype="multipart/form-data">
 First Name:<input type="text" name="Fname" value="<?php echo $query2['Fname']; ?>" /><br /><br />
 Last Name:<input type="text" name="Lname" value="<?php echo $query2['Lname']; ?>" /><br /><br />
 Phone Number:<input type="text" name="PhoneNumber" value="<?php echo $query2['PhoneNumber']; ?>" /><br /><br />
 Salary:<input type="text" name="Salary" value="<?php echo $query2['Salary']; ?>" /><br /><br />
-JobTitle:<input type="text" name="JobTitle" value="<?php echo $query2['JobTitle']; ?>" /><br /><br />
-
+Job Title:<input type="text" name="JobTitle" value="<?php echo $query2['JobTitle']; ?>" /><br /><br />
+Image: <input type="file" name="image" value="<?php echo $row['name']; ?>" /><br /><br />
 <br />
 <input type="submit" name="submit" value="update" />
 </form>
