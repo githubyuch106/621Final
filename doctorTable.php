6e6fa974dd9b94341eae79af2cd385969a119121
sethfields23 <sethfields23@gmail.com>
2016-12-04 16:04:39 -0500

    Update doctorTable.php
    Fixed errors with this page and added session tracking.

--- doctorTable.php
+++ doctorTable.php
@@ -1,17 +1,13 @@
 <?php
 	session_start();
-	
-	//Make sure page cant be accessed without logging in
-	if(!isset($_SESSION['EmpID'])) {
-        header("Location: home.html");
-    }
+	require 'doctorSession.php';
 
-	$EmpID = $_SESSION['EmpID'];
+	$EmpID = $_SESSION['aEmpID'];
 	$search = $_REQUEST["search"];
 	$searchBy = $_REQUEST["searchBy"];
 	
 	$query = "SELECT  PatientID , Fname , Lname , BloodType , Sex , Weight , Height , Vitals  from patient where EmpID = '$EmpID' and $searchBy like '%$search%'";
-    $result= buildTable($query);
+        $result= buildTable($query);
 	print($result);
 
 	function buildTable($query){
@@ -38,35 +34,43 @@
 
 
 		//*** return query results in a string
-		$query_result = "<table border=0 cellspacing=10  width=500 align=left>\n";
+		$query_result = "<table class='zui-table zui-table-zebra zui-table-horizontal' border=0 cellspacing=10  width=500 align=left>\n";
 
 		$query_result = $query_result . "<tr align=Center>";
 		for ($i = 0; $i<mysql_num_fields($result); $i++)
 			$query_result = $query_result . "<th style=\"color: black\">" . ucfirst(mysql_field_name($result, $i)) . "</th>";
 		$query_result = $query_result . "</tr>";
+		$check=$query_result;
 
-              //*** background colors for printing
-		$ptr = 0;
-		$colors = array("white", "#dcebf8");
+		$count=0;
 
-		while ($row = mysql_fetch_row($result)) {
-
-			//*** alternate colors
-            $ptr = ($ptr + 1)%2;
-	        $query_result = $query_result . "<tr bgcolor=$colors[$ptr]>\n";
+		while ($row = mysql_fetch_row($result)) {	
+	
+	        $query_result = $query_result . "<tr bgcolor='white'>\n";
 
 			foreach ($row as $item) {
+				if($count==0){
+					$id = $item;
+				}
+
+				$count++;
 				$query_result = $query_result . "   <td>$item</td>\n";
 	         }
-
-
-	        $query_result = $query_result . "<td colspan='2'>". "<a href = 'DoctorEditPatient.php?Edit=$row[PatientID]'>Edit</a>".  "</td>";
-	   		$query_result = $query_result ."<td colspan='2'>". "<a href = 'DoctorAddReport1.php?Report=$row[PatientID]'>Reports</a>".  "</td>";
+	        $count=0;
+	        $query_result = $query_result . "<td colspan='2'>". "<a href = 'DoctorEditPatient.php?Edit=".$id."'>Edit</a>".  "</td>";
+	   		$query_result = $query_result ."<td colspan='2'>". "<a href = 'DoctorAddReport1.php?Report=".$id."'>Reports</a>".  "</td>";
 		  	
 		  	$query_result = $query_result . "</tr>\n";
 		}
 
 		$query_result = $query_result . "</table>\n";
+		$check=$check."</table>\n";
+
+		if($check == $query_result){
+
+			$result = "<br><p>Search Not found<p/>";
+			return $result;
+		}
 
 
 
