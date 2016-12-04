259f7e005e159b469a52056e1e67a94e9643f2fe
sethfields23 <sethfields23@gmail.com>
2016-12-04 16:00:00 -0500

    Updated doctor.php with my code
    Updated doctor.php with my code

--- doctor.php
+++ doctor.php
@@ -1,104 +1,119 @@
-<html>
-<body>
-	
-	<form action="logout.php">
-    <input type="submit" value="logout" />
-</body>
-</html>
-
 <?php
-session_start();
-require 'common.php';
-$connection = new mysqli($localhost , $dusername , $dpassword,$database);
-if ($connection->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-	echo "No Connection to DB";
-} 
-//echo "Connected successfully";
-echo "<pre>";
 
-$aEmpID = $_SESSION['aEmpID'];
-//echo $aEmpID;
+    session_start();
+ 	require 'doctorSession.php';
 
-$FineEmpName = mysqli_query($connection,"SELECT Fname , Lname FROM employee WHERE EmpID = '$aEmpID'");
- $row = mysqli_fetch_array($FineEmpName,MYSQLI_ASSOC);
-   
- $EmpFname = $row['Fname'];
- $EmpLname = $row['Lname'];
-   
-   echo "Welcome Dr. ";
-   echo $EmpFname;
-   echo " ";
-   echo $EmpLname;
-   
+    //Make sure page cant be accessed without logging in
+    if(!isset($_SESSION['aEmpID'])) {
+    	echo "<script  type='text/javascript'>
+                    alert('Access Denied');
+                    window.location = 'index.html';
+                </script>";
+        header("Location: index.html");
 
-echo "<pre>";
- 
-	 $quiry = "SELECT  PatientID , Fname , Lname , BloodType , Sex , Weight , Height , Vitals  from patient where EmpID = '$aEmpID'";
-
-	 $result = mysqli_query($connection, $quiry);
-//echo $result;
-echo "<pre>";
-if (!$result)
-{
-	
-    die("Query Faile".  mysqli_errno($connection));   
-}
-
-if ($result->num_rows > 0) {
-    // output data of each row
-	echo "<table align='left' width='20%' height='20%'>";
-	
-	   echo "<tr>";
-	   echo "<td>PatientID</td>";
-       echo "<td>First Name</td>";
-	   echo "<td>Last Name</td>";
-	   echo "<td>BloodType</td>";
-	   echo "<td>Sex</td>";
-	   echo "<td>Weight</td>";
-	   echo "<td>Height</td>";
-	   echo "<td>Vitals</td>";
-		echo "</tr>";
-    while($row = $result->fetch_assoc()) {
-	   $PatientID = $row['PatientID'];
-       $Fname =  $row['Fname'];
-	   $Lname =  $row['Lname'];
-	   $BloodType =  $row['BloodType'];
-	   $Sex =  $row['Sex'];
-	   $Weight =  $row['Weight'];
-	   $Height =  $row['Height'];
-	   $Vitals =  $row['Vitals'];
-
-		
-       echo "<tr>";
-       echo "<td>$PatientID</td>";
-       echo "<td>$Fname</td>";
-	   echo "<td>$Lname</td>";
-	   echo "<td>$BloodType</td>";
-	   echo "<td>$Sex</td>";
-	   echo "<td>$Weight</td>";
-	   echo "<td>$Height</td>";
-	   echo "<td>$Vitals</td>";
-	   echo "<td colspan='2'>". "<a href = 'DoctorEditPatient.php?Edit=$row[PatientID]'>Edit</a>".  "</td>";
-	   echo "<td colspan='2'>". "<a href = 'DoctorAddReport1.php?Report=$row[PatientID]'>Add Report</a>".  "</td>";
-	  // echo "<td colspan='2'>". "<a href = 'DoctorAddReport1.php?Report=$row[PatientID]'>List Reports</a>".  "</td>";
-	   //echo "<br>";
-	   echo "</tr>";
-		
     }
-} else {
-	
-		echo "There is no patients";
-		//header("location:regist.html");
-
-    //echo "0 results";
-	echo "</table>";
-}
-
-
-
-
-
-
 
 ?>
+
+<html>
+
+    <head>
+    	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Hospital</title>
+        <link href="styles.css" rel="stylesheet" type="text/css" />
+        <link href="drop.css" rel="stylesheet" type="text/css" />
+        <script  type="text/javascript" >
+
+            var xmlhttp = new XMLHttpRequest();
+            
+            xmlhttp.onreadystatechange=function() {
+
+                       //*** response is ready
+                       if ((xmlhttp.readyState == 4) && (xmlhttp.status == 200)) {
+
+                            document.getElementById("patientTable").innerHTML = xmlhttp.responseText;
+                       }
+
+             }   
+
+             function ajaxFunction() {
+
+                  var URL = "http://localhost:8888/621Final-master//doctorTable.php";
+                  var queryString = "searchBy=" + document.getElementById("searchBy").value +"&search="+ document.getElementById("search").value;
+
+                  xmlhttp.open("POST", URL, true);
+                  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                  xmlhttp.send(queryString);
+
+              }
+
+        </script>
+
+    </head>
+
+    <body>
+
+	    	<div id="container">
+	  <!-- begin #header -->
+	    <div id="header">
+	    <div class="headerTop">
+	          <div class="logo">
+	              <a href=""><img src="images/logo.png" alt="" width="179" height="77" /></a>AMS <span>the best technologies</span>
+	            </div>
+	            <div class="search">
+	                Product search:<br />
+	                <div class="si"><input name="" type="text" /></div><div><a href=""><img src="images/searchButton.jpg" alt="" width="66" height="26" /></a></div>
+	            </div>
+	     </div>
+	     <div class="mainMenu">
+	      <ul class="menuTemplate1 decor1_1" license="mylicense">
+			    <li class="separator"></li>
+
+			    <li><a href="#Horizontal-Menus" class="arrow">Settings</a>
+			        <div class="drop decor1_2" style="width: 150px;">
+			            <div class='left'>
+			                <div>
+			                    <a href="settings.php">Change Password</a><br/>
+			                    <a href="Edit.php">Edit Doctor Info</a><br/>
+			                </div>
+			            </div>
+			      
+			    <li><a href="logout.php" class="arrow">Logout</a>
+			    
+
+	     	 </ul>
+	        
+	    </div>
+
+	    <!-- end #header -->
+
+	    <h2>Doctor Page</h2>
+	    <br>
+        <form name="PaitentTableFrom">
+
+            <input type="text" id="search" placeholder="Search for Patients" onKeyUp="ajaxFunction()"/>
+            
+            <select id="searchBy">
+                <option value="Fname">First Name</option>
+                <option value="Lname">Last Name</option>
+                <option value="Bloodtype">Bloodtype</option>
+                <option value="Sex">Sex</option>
+                <option value="Weight">Weight</option>
+                <option value="Height">Height</option>
+                <option value="Vitals">Vitals</option>
+            </select> 
+            
+        </form>
+
+        <div class="table" id="patientTable">
+        </div>
+
+           </div>
+      <!-- This clearing element should immediately follow the #mainContent div in order to force the #container div to contain all   child floats --><br class="clearfloat" />
+      <!-- begin #footer -->
+
+      <!-- end #footer -->
+      </div>
+    </body>
+
+</html>
