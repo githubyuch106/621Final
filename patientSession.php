8e1d7189484b1af14b7b8ade24636ed6740792b9
sethfields23 <sethfields23@gmail.com>
2016-12-04 20:53:47 -0500

    Update patientSession.php

--- patientSession.php
+++ patientSession.php
@@ -2,7 +2,7 @@
 	//Make sure page cant be accessed without logging in
 	if(!isset($_SESSION['PatientID'])) {
 		echo "<script  type='text/javascript'>
-	                alert('Access Denied. Please Sign in as a doctor');
+	                alert('Access Denied. Please Sign in as a patient.');
 	                window.location = 'index.html';
 	            </script>";
 	    header("Location: index.html");
