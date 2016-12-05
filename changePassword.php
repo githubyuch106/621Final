bce77deb2bc458bf1a592a6a93d199186554db0c
sethfields23 <sethfields23@gmail.com>
2016-12-04 19:12:06 -0500

    Update changePassword.php

--- changePassword.php
+++ changePassword.php
@@ -1,27 +1,19 @@
 <?php
-        session_start();
-        require 'doctorSession.php';
+    session_start();
+	require 'doctorSession.php';
 
 	$empID = $_SESSION['aEmpID'];
 	$pword = $_POST['password'];
 
-	
-	$localhost = 'localhost';
-        $username = 'root';
-	$password = 'root';
-	$database = 'hospital';
-
 	$query = "UPDATE employee SET Password = '$pword' WHERE EmpID = '$empID'";
 
-	//*** create a connection object
-	$connection = mysql_connect($localhost, $username, $password)
-			or die (mysql_error());
-
-	mysql_select_db($database)
-			or die (mysql_error());
-
-	//*** execute the query
-	$result = mysql_query($query);
+	require 'common.php';
+	$connection = new mysqli($localhost , $dusername , $dpassword,$database);
+	if ($connection->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+		echo "No Connection to DB";
+	} 
+	
 	$_SESSION['PassChanged'] = 1;
 
 	header("Location: settings.php");
