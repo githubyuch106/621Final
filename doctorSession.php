8beb1a4e94aefc4d4dae3aaaa37ec5ecf6ec1eac
sethfields23 <sethfields23@gmail.com>
2016-12-04 20:54:18 -0500

    Update doctorSession.php

--- doctorSession.php
+++ doctorSession.php
@@ -2,7 +2,7 @@
 	//Make sure page cant be accessed without logging in
 	if(!isset($_SESSION['aEmpID'])) {
 		echo "<script  type='text/javascript'>
-	                alert('Access Denied. Please Sign in as a doctor');
+	                alert('Access Denied. Please Sign in as a doctor.');
 	                window.location = 'index.html';
 	            </script>";
 	    header("Location: index.html");
