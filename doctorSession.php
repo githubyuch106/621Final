3b7334a3217407fc99df41e6ff75ccf4f41b6756
sethfields23 <sethfields23@gmail.com>
2016-12-05 19:13:54 -0500

    Update doctorSession.php

--- doctorSession.php
+++ doctorSession.php
@@ -1,8 +1,7 @@
 <?php
 
-	session_destroy();
-
 	if(!isset($_SESSION['aEmpID'])) {
+		session_destroy();
 		echo "<script  type='text/javascript'>
 	                alert('Access Denied. Please Sign in as a Doctor.');
 	                window.location = 'index.html';
