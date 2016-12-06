e3ca74172d44a0c9537bd532e9cce09926f230a4
sethfields23 <sethfields23@gmail.com>
2016-12-05 19:13:35 -0500

    Update patientSession.php

--- patientSession.php
+++ patientSession.php
@@ -1,7 +1,7 @@
 <?php
-	session_destroy();
 
 	if(!isset($_SESSION['PatientID'])) {
+		session_destroy();
 		echo "<script  type='text/javascript'>
 	                alert('Access Denied. Please Sign in as a Patient.');
 	                window.location = 'index.html';
